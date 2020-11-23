83cf46857e294fd78290435b18245633dfb52408
Amber Joy <amber.joy.dev@gmail.com>
2020-11-22 23:05:50 -0600

    initial commit w/ photos

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <InstaModo msg="Insta-Quasi-Clone-Modo" :photos="photos" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueFilterDateFormat from "@vuejs-community/vue-filter-date-format";
+import sortBy from "lodash/sortBy";
+import * as metadata from "@/assets/media.json";
+import InstaModo from "./components/InstaModo.vue";
+
+Vue.use(VueFilterDateFormat);
+
+export default {
+  name: "App",
+  components: {
+    InstaModo,
+  },
+  data: () => ({
+    photos: sortBy(metadata.photos, "taken_at"),
+    videos: metadata.videos,
+    dateFormatConfig: {
+      dayOfWeekNames: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      dayOfWeekNamesShort: ["Su", "Mo", "Tu", "We", "Tr", "Fr", "Sa"],
+      monthNames: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      monthNamesShort: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      // Timezone offset, in minutes (0 - UTC, 180 - Russia, undefined - current)
+      timezone: 0,
+    },
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InstaModo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="all">
+    <h1>{{ msg }}</h1>
+    <div v-for="photo in photos" v-bind:key="photo.path" width="20%">
+      <figure>
+        <img v-bind:src="'/assets/' + photo.path" />
+        <figcaption>
+          {{
+            new Date(photo.taken_at)
+              | dateFormat("dddd, MMMM DD, YYYY h:mm a", dateFormatConfig)
+          }}
+          {{ photo.caption }}<br />
+        </figcaption>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InstaModo",
+  props: {
+    msg: String,
+    photos: Array,
+    dateFormatConfig: Object
+  }
+};
+</script>
